802b4d97a31adb54f7fefe76348bf86494c7a12f
Lee Pham <leepham@utexas.edu>
2022-08-14 16:19:53 -0500

    randomize rotation direction

--- wind.py
+++ wind.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 from ev3dev2.motor import MediumMotor, OUTPUT_B, SpeedPercent
+from random import random
+
 
 WATCH_ROTATIONS_PER_HOUR = 50
 gear_ratio = 56/40
 motor_rotations = WATCH_ROTATIONS_PER_HOUR * gear_ratio
 
 motor = MediumMotor(OUTPUT_B)
-motor.on_for_rotations(SpeedPercent(10), motor_rotations)
+direction =  1 if random.random() < .5 else -1
+motor.on_for_rotations(SpeedPercent(15 * direction), motor_rotations)
