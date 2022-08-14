39930670b7f9b98716fb4f1595d5f877598b8ba5
Lee Pham <leepham@utexas.edu>
2022-08-14 14:18:41 -0500

    account for gear ration

--- wind.py
+++ wind.py
@@ -2,5 +2,9 @@
 
 from ev3dev2.motor import MediumMotor, OUTPUT_B, SpeedPercent
 
+WATCH_ROTATIONS_PER_HOUR = 50
+gear_ratio = 56/40
+motor_rotations = WATCH_ROTATIONS_PER_HOUR * gear_ratio
+
 motor = MediumMotor(OUTPUT_B)
-motor.on_for_rotations(SpeedPercent(10), 5)
+motor.on_for_rotations(SpeedPercent(10), motor_rotations)
