3b31389da76aad844028acbc5575b5bf229d136b
Lee Pham <leepham@utexas.edu>
2022-08-14 13:53:09 -0500

    lower speed

--- wind.py
+++ wind.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from time import sleep
-from ev3dev2.motor import MediumMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.motor import MediumMotor, OUTPUT_B, SpeedPercent
 
 motor = MediumMotor(OUTPUT_B)
-motor.on_for_rotations(SpeedPercent(75), 5)
+motor.on_for_rotations(SpeedPercent(10), 5)
