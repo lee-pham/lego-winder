59e4bf61784a2247ed251dce63c082f075372d75
Lee Pham <leepham@utexas.edu>
2022-08-14 21:50:37 +0000

    allow motor to coast instead of brake

--- wind.py
+++ wind.py
@@ -10,4 +10,4 @@ motor_rotations = WATCH_ROTATIONS_PER_HOUR * gear_ratio
 
 motor = MediumMotor(OUTPUT_B)
 direction = 1 if random() < .5 else -1
-motor.on_for_rotations(SpeedPercent(15 * direction), motor_rotations)
+motor.on_for_rotations(SpeedPercent(15 * direction), motor_rotations, brake=False)
