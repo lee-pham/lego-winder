e8ef48855ff8d33bd89a784a816e18582ba87f05
Lee Pham <leepham@utexas.edu>
2022-08-20 01:58:36 -0400

    convert from ev3dev to buildhat

--- wind.py
+++ wind.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
-
-from ev3dev2.motor import MediumMotor, OUTPUT_B, SpeedPercent
-from random import random
+import buildhat
+import random
 
 
 WATCH_ROTATIONS_PER_HOUR = 50
 gear_ratio = 56/40
 motor_rotations = WATCH_ROTATIONS_PER_HOUR * gear_ratio
 
-motor = MediumMotor(OUTPUT_B)
-direction = 1 if random() < .5 else -1
-motor.on_for_rotations(SpeedPercent(15 * direction), motor_rotations, brake=False)
+motor = buildhat.Motor("C")
+direction = 1 if random.random() < .5 else -1
+motor.run_for_rotations(motor_rotations, speed=direction*100, blocking=True)
