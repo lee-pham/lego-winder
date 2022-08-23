f30b0c8ded2aa160d92137b39ed8d420006fe0f8
Lee Pham <leepham@utexas.edu>
2022-08-23 00:15:29 -0500

    adjusting speed led to motor not running issues

--- wind.py
+++ wind.py
@@ -3,9 +3,10 @@ import random
 
 
 WATCH_ROTATIONS_PER_HOUR = 50
+MOE = .85
 gear_ratio = 60/36
 motor_rotations = WATCH_ROTATIONS_PER_HOUR * gear_ratio
 
 motor = buildhat.Motor("C")
 direction = 1 if random.random() < .5 else -1
-motor.run_for_rotations(1, speed=direction*100)
+motor.run_for_rotations(direction * motor_rotations / MOE)
