5cfd68dc6aa64fa631770b5e8664e13ef7d0fdf4
Lee Pham <leepham@utexas.edu>
2022-08-23 23:41:38 -0500

    geared for more torque

--- wind.py
+++ wind.py
@@ -4,8 +4,9 @@ import random
 
 WATCH_ROTATIONS_PER_HOUR = 50
 MOE = .85
-gear_ratio = 60/36
-motor_rotations = WATCH_ROTATIONS_PER_HOUR * gear_ratio
+gear_ratio_1 = 60/36
+gear_ratio_2 = 36/12
+motor_rotations = WATCH_ROTATIONS_PER_HOUR * gear_ratio_1 * gear_ratio_2
 
 motor = buildhat.Motor("C")
 direction = 1 if random.random() < .5 else -1
