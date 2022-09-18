946867a2107ce254af224137a95a09befcfa7bdc
Lee Pham <leepham@utexas.edu>
2022-09-17 23:42:18 -0500

    incorporate sleep mode by utilizing color sensor

--- wind.py
+++ wind.py
@@ -9,5 +9,9 @@ gear_ratio_2 = 36/12
 motor_rotations = WATCH_ROTATIONS_PER_HOUR * gear_ratio_1 * gear_ratio_2
 
 motor = buildhat.Motor("C")
-direction = 1 if random.random() < .5 else -1
-motor.run_for_rotations(direction * motor_rotations / MOE)
+color = buildhat.ColorSensor('D')
+
+if color.wait_for_new_color() == "white":
+    color.off()
+    direction = 1 if random.random() < .5 else -1
+    motor.run_for_rotations(direction * motor_rotations / MOE)
